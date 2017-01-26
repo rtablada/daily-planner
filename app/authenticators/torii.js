4284d642a12c8ba2b3fecd54279b5ce816f875fc
Ryan Tablada <ryan.tablada@gmail.com>
2017-01-26 12:34:43 -0600

    fix: Reject failed authentication

--- app/authenticators/torii.js
+++ app/authenticators/torii.js
@@ -2,6 +2,14 @@ import Ember from 'ember';
 import ToriiAuthenticator from 'ember-simple-auth/authenticators/torii';
 import config from 'daily-planner/config/environment';
 
+function parseJson(r) {
+  if (r.ok) {
+    return r.json();
+  }
+
+  return r.json().then(data => Promise.reject(data));
+}
+
 export default ToriiAuthenticator.extend({
   torii: Ember.inject.service(),
 
@@ -13,7 +21,7 @@ export default ToriiAuthenticator.extend({
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ 'auth-code': data.authorizationCode })
-    }).then(r => r.json())
+    }).then(parseJson)
       .then(res => ({
         access_token: res.token,
         provider: data.provider
