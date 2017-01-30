85881d0c81da5dcf9dada64fc7e11bdf3dce6e2b
Ryan Tablada <ryan.tablada@gmail.com>
2017-01-30 13:26:24 -0600

    fix: moment utc for lesson date

--- app/models/lesson.js
+++ app/models/lesson.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  date: DS.attr('date'),
+  date: DS.attr('moment-utc'),
   visible: DS.attr('boolean'),
 
   week: DS.attr('string'),
