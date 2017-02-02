389091468f4d6735a1d5ec78fbcf549ed20fafbd
Ryan Tablada <ryan.tablada@gmail.com>
2017-02-01 23:27:24 -0600

    feat: auto transition to cohort for students

--- app/dashboard/cohort/index/route.js
+++ app/dashboard/cohort/index/route.js
@@ -3,12 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   beforeModel() {
     const user = this.modelFor('dashboard');
-    user.get('student').then((s) => {
-      debugger;
-    })
 
     if (!user.get('isAdmin')) {
-      return user.get('student.cohort')
+      return user.get('student')
+        .then(student => student.get('cohort'))
         .then((cohort) => {
           this.transitionTo('dashboard.cohort.detail', cohort.id);
         });
